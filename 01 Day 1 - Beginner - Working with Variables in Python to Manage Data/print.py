eb4324a3888f24b17a61a512a001a5ddf9882fcb
Ashish Sahu <ashsahu9713@gmail.com>
2022-08-30 18:05:15 +0530

    Day 1

--- 01 Day 1 - Beginner - Working with Variables in Python to Manage Data/print.py
+++ 01 Day 1 - Beginner - Working with Variables in Python to Manage Data/print.py
@@ -3,3 +3,7 @@ print("100 Days of Python")
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+print("100 Days of Python \n100 Days of Python")
+
+print("Day" + "1")
