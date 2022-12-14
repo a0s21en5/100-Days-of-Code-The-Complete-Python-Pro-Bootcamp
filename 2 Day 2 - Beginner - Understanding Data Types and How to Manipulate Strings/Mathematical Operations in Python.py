6e1955934520a1c4e7f3f3a74ceaa1ed8399ad1a
Ashish Sahu <86140629+a0s21en5@users.noreply.github.com>
2022-12-14 11:48:15 +0000

    Update

---
+++ 2 Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/Mathematical Operations in Python.py
@@ -0,0 +1,16 @@
+first_num = 5
+second_num = 3
+
+print("Add = ", first_num + second_num)
+
+print("Sub = ", first_num - second_num)
+
+print("Multi = ", first_num * second_num)
+
+print("Div = ", first_num / second_num)
+
+print("Module= ", first_num % second_num)
+
+print("Floor = ", first_num // second_num)
+
+print("Expo = ", first_num**second_num)
