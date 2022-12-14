f0349d4badd467fe6e6226269d8588556f6e9838
Ashish Sahu <86140629+a0s21en5@users.noreply.github.com>
2022-12-14 10:56:59 +0000

    Update

--- 2 Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/Type Error, Type Checking and Type Conversion.py
+++ 2 Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/Type Error, Type Checking and Type Conversion.py
@@ -1,2 +1,7 @@
 #Type Error, Type Checking and Type Conversion
+num_char = len(input("Enter Your Name: "))
+#print(type(len(input("Enter Your Name: "))))
 
+new_num_char = str(num_char)
+
+print("Your Name Contains " + new_num_char + " Chacters")
